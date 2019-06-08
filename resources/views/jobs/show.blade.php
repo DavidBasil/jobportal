580d897f8532f9149c2e831595789ae902c40cf1
David Basil <info@basildavid.com>
2019-06-08 16:45:16 +0400

    add dynamic data to show.blade

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -6,17 +6,29 @@
 
 			<div class="col-md-8">
 				<div class="card">
-					<div class="card-header">Jobs Title</div>
-
-					<div class="card-body"></div>
+					<div class="card-header">{{ $job->title }}</div>
+					<div class="card-body">
+						<p>
+						<h3>Description</h3>
+						{{ $job->description }}</p>
+						<hr>
+						<p>
+						<h3>Duties</h3>
+						{{ $job->roles }}</p>
+					</div>
 				</div>
 			</div>
 
 			<div class="col-md-4">
 				<div class="card">
 					<div class="card-header">Short Info</div>
-
-					<div class="card-body"></div>
+					<div class="card-body">
+						<p>Company:</p>
+						<p>Address: {{ $job->address }}</p>
+						<p>Employment Type: {{ $job->type }}</p>
+						<p>Position: {{ $job->position }}</p>
+						<p>Date: {{ $job->created_at->diffForHumans() }}</p>
+					</div>
 				</div>
 			</div>
 
