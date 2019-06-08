247df138e89861ddd27b1d5a2da3d51d7616c015
David Basil <info@basildavid.com>
2019-06-08 14:11:54 +0400

    add fields to Jobs table

--- database/migrations/2019_06_08_093302_create_jobs_table.php
+++ database/migrations/2019_06_08_093302_create_jobs_table.php
@@ -15,6 +15,17 @@ class CreateJobsTable extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
+						$table->string('user_id');
+						$table->string('company_id');
+						$table->string('title');
+						$table->string('slug');
+						$table->text('description');
+						$table->integer('category_id');
+						$table->integer('position');
+						$table->string('address');
+						$table->string('type');
+						$table->integer('status');
+						$table->date('last_date');
             $table->timestamps();
         });
     }
