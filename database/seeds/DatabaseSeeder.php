6e24451dbc59374558505bc635e0de6deafb8b77
David Basil <info@basildavid.com>
2019-06-08 14:45:28 +0400

    add factories to DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+			factory('App\User', 20)->create();
+			factory('App\Company', 20)->create();
+			factory('App\Job', 20)->create();
     }
 }
