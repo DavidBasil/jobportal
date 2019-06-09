8b98a36497cfefb19c063661eb627b977baa2ed1
David Basil <info@basildavid.com>
2019-06-09 17:13:19 +0400

    add cover letter update functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,23 @@ class UserController extends Controller
 			'experience' => request('experience'),
 			'bio' => request('bio')
 		]);
-		return redirect()->back()->with('message', 'Profile Updated');
+		return redirect()->back()->with('profile_message', 'Profile Updated');
+	}
+
+	public function coverletter(Request $request){
+		$user_id = auth()->user()->id;
+		/* $cover = $request->file('cover_letter')->store('public/files'); */
+		$cover = $request->file('cover_letter');
+		if(isset($cover)){
+			$cover->store('public/files');
+			Profile::where('user_id', $user_id)->update([
+			'cover_letter' => $cover	
+			]);
+			return redirect()->back()->with('cover_letter_message', 'Cover letter updated')->with('class', 'alert alert-success');
+		} else {
+			return redirect()->back()->with('cover_letter_message', 'Choose a file')->with('class', 'alert alert-danger');
+		}
+
 	}
 
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -35,9 +35,9 @@
 						</div>
 
 					{{-- session message --}}
-					@if(Session::has('message'))
+					@if(Session::has('profile_message'))
 						<div class="alert alert-success w-100 text-center">
-							{{ Session::get('message') }}
+							{{ Session::get('profile_message') }}
 						</div>
 					@endif
 
@@ -64,9 +64,16 @@
 			<div class="card mt-2">
 				<div class="card-header">Update Coverletter</div>
 				<form action="{{ route('cover.letter') }}" method="post" enctype="multipart/form-data">
+					@csrf
 					<div class="card-body">
 						<input type="file" name="cover_letter" class="form-control">
 						<button class="btn btn-success w-100 mt-2" type="submit">Update</button>
+						{{-- message --}}
+						@if(Session::has('cover_letter_message'))
+							<div class="{{ Session::get('class') }} mt-2">
+								{{ Session::get('cover_letter_message') }}
+							</div>
+						@endif
 					</div>
 				</form>
 			</div>
