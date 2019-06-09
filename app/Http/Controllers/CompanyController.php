3fe269eb9e51e22fd9d1ccdbbed1de4c96b24c75
David Basil <info@basildavid.com>
2019-06-09 13:25:27 +0400

    add show method to Company controller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Company;
 
 class CompanyController extends Controller
 {
-    //
+
+	public function show($id, Company $name){
+		return view('company.show', compact('name'));
+	}
+
 }
