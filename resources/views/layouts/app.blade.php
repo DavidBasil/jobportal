2c23d992f4696b0f26b4fd5d4716487385237c6f
David Basil <info@basildavid.com>
2019-06-11 19:21:25 +0400

    edit app.blade for user name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,11 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+																	@if(Auth::user()->user_type == 'employer')
+																		{{ Auth::user()->company->cname }} <span class="caret"></span>
+																	@else
+																		{{ Auth::user()->name }} <span class="caret"></span>
+																	@endif
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
