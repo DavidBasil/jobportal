71887dd19040a96cc53b589ae2867c3128672281
David Basil <info@basildavid.com>
2019-06-15 12:53:46 +0400

    add new middlewares to Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'employer' => 'App\Http\Middleware\Employer',
+        'seeker' => 'App\Http\Middleware\Seeker'
     ];
 
     /**
