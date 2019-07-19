3ab75d40a3594f60ab02c50c304c8ac3dc160c8e
David Basil <info@basildavid.com>
2019-07-19 19:40:11 +0400

    add now-ui-kit to front-end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,37 +7,34 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name', 'Laravel') }}</title>
   <!-- Scripts -->
-  <script src="{{ asset('js/app.js') }}" defer></script>
+  {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
   <!-- Styles -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/solid.min.css" type="text/css">
+  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/now-ui-kit.min.css') }}" rel="stylesheet">
+  {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/solid.min.css" type="text/css"> --}}
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/fontawesome.min.css" type="text/css">
 </head>
 <body>
 <div id="app">
-  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-md navbar-light sticky-top bg-primary">
     <div class="container">
       <a class="navbar-brand" href="{{ url('/') }}">
-        {{ config('app.name', 'Laravel') }}
+        <img src="{{ asset('avatar/logo.png') }}" alt="">
       </a>
       <button 
-        class="navbar-toggler" 
-        type="button" 
+        class="navbar-toggler navbar-toggler-icon" 
         data-toggle="collapse" 
-        data-target="#navbarSupportedContent" 
-        aria-controls="navbarSupportedContent" 
-        aria-expanded="false" 
-        aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
+        data-target="#navbarSupportedContent">
+        {{-- <span class="navbar-toggler-icon"></span> --}}
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav mr-auto">
-        </ul>
         <!-- Right Side Of Navbar -->
         <ul class="navbar-nav ml-auto">
           <!-- Authentication Links -->
@@ -125,5 +122,8 @@
 
 </div>
   {{-- end of app div --}}
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/popper.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </body>
 </html>
